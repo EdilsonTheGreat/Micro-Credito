376d0c36329fb6f3347af8a8279c6f02291e1d93
Edilson Manuel <edilsonmanuel.academic@gmail.com>
2025-09-27 16:58:58 +0200

    Update DBConnection.java

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection{
-    private static final String URL = "jdbc:mysql://localhost:3306/microcredito";
-    private static final String USER = "root";
-    private static final String PASSWORD = "952538Edilson";
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
     private static  Connection connection = null;
 
     //metodo para obter coneão
